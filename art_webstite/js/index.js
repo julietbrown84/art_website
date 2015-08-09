4a5d16baf4e8d3e08c3d5d0134167d8eb8aecedb
Juliet Brown <julietbrown84@gmail.com>
2015-08-09 22:12:13 +1200

    Updated draggable js function and file

--- art_webstite/js/index.js
+++ art_webstite/js/index.js
@@ -1,6 +1,5 @@
-$(".button").click(function() {
-    $("#box1").css('margin-left', 0);
-    $("#box1").css('margin-right', 0);
-    $("#box1").animate({ left: '-500px' }, 'slow');
-    $("#box2").animate({ left: '0px' }, 'slow');
+$(document).ready(function () {  
+	 $(function() {
+	    $( ".portrait" ).draggable();
+	  });
 });
